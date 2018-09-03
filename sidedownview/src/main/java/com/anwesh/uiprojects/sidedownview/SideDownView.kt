d32fd781356ca25f73c32bdae6c6aaf5dc51954f
Anwesh43 <anweshthecool0@gmail.com>
2018-09-04 02:09:15 +0530

    adding State class

--- sidedownview/src/main/java/com/anwesh/uiprojects/sidedownview/SideDownView.kt
+++ sidedownview/src/main/java/com/anwesh/uiprojects/sidedownview/SideDownView.kt
@@ -28,4 +28,24 @@ class SideDownView(ctx : Context) : View(ctx) {
         }
         return true
     }
+
+    data class State(var scale : Float = 0f, var dir : Float = 0f, var prevScale : Float = 0f) {
+
+        fun update(cb : (Float) -> Unit) {
+            scale += 0.05f * this.dir
+            if (Math.abs(scale - prevScale) > 1) {
+                scale = prevScale + dir
+                dir = 0f
+                prevScale = scale
+                cb(prevScale)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            if (dir == 0f) {
+                dir = 1 - 2 * prevScale
+                cb()
+            }
+        }
+    }
 }
