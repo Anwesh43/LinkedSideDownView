33289b979b1da81e02025bc8074139403187b2f2
Anwesh43 <anweshthecool0@gmail.com>
2018-09-04 02:40:59 +0530

    adding SideDownView in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedsidedownview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedsidedownview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedsidedownview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.sidedownview.SideDownView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        SideDownView.create(this)
     }
 }
