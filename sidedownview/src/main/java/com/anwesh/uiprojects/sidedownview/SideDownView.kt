ef45f57d9fb90e3a9df189908efa53bf1420c15b
Anwesh43 <anweshthecool0@gmail.com>
2018-09-04 02:04:42 +0530

    adding libraries and defining nodes

--- sidedownview/src/main/java/com/anwesh/uiprojects/sidedownview/SideDownView.kt
+++ sidedownview/src/main/java/com/anwesh/uiprojects/sidedownview/SideDownView.kt
@@ -3,3 +3,11 @@ package com.anwesh.uiprojects.sidedownview
 /**
  * Created by anweshmishra on 04/09/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.content.Context
+
+val nodes : Int = 5
